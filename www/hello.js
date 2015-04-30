070a4d314a7e2d5ac58be073f5a359084cab9927
cianclarke <cian@cianclarke.com>
2015-04-30 16:18:17 -0400

    Integrate with our UPS service

--- www/hello.js
+++ www/hello.js
@@ -4,11 +4,13 @@ document.getElementById('say_hello').onclick = function () {
       {
         path: 'hello',
         data: {
-          hello: document.getElementById('hello_to').value
+          barcode: document.getElementById('hello_to').value
         }
       },
       function (res) {
-        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
+        var product  = res[0];
+        document.getElementById('cloudResponse').innerHTML = "<img src='" + product.imageurl + "'>" + 
+        "<p>" + product.productname + "</p>";
       },
       function (code, errorprops, params) {
         alert('An error occured: ' + code + ' : ' + errorprops);
