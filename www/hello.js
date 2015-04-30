e7a2b0e85a85042d4e8e13a30584fa13bd598fed
cianclarke <cian@cianclarke.com>
2015-04-30 16:24:35 -0400

    Add barcode scanner code

--- www/hello.js
+++ www/hello.js
@@ -1,19 +1,24 @@
 document.getElementById('say_hello').onclick = function () {
-  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
-  $fh.cloud(
-      {
-        path: 'hello',
-        data: {
-          barcode: document.getElementById('hello_to').value
+  return cordova.plugins.barcodeScanner.scan(function (result) {
+    document.getElementById('hello_to').value = result.text;
+    document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
+    $fh.cloud(
+        {
+          path: 'hello',
+          data: {
+            barcode: result.text
+          }
+        },
+        function (res) {
+          var product  = res[0];
+          document.getElementById('cloudResponse').innerHTML = "<img src='" + product.imageurl + "'>" + 
+          "<p>" + product.productname + "</p>";
+        },
+        function (code, errorprops, params) {
+          alert('An error occured: ' + code + ' : ' + errorprops);
         }
-      },
-      function (res) {
-        var product  = res[0];
-        document.getElementById('cloudResponse').innerHTML = "<img src='" + product.imageurl + "'>" + 
-        "<p>" + product.productname + "</p>";
-      },
-      function (code, errorprops, params) {
-        alert('An error occured: ' + code + ' : ' + errorprops);
-      }
-  );
+    );
+  }, function (error) {
+    alert("Scanning failed: " + error);
+  });
 };
